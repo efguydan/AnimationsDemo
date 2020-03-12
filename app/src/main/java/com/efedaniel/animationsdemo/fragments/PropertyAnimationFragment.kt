06f9c3019e01deb37ebe4cc4ed0613ea24a6d659
EfeDaniel <EfeEjemudaro@gmail.com>
2020-03-13 00:53:00 +0100

    Feature: add menu options for controlling animator

--- app/src/main/java/com/efedaniel/animationsdemo/fragments/PropertyAnimationFragment.kt
+++ app/src/main/java/com/efedaniel/animationsdemo/fragments/PropertyAnimationFragment.kt
@@ -4,10 +4,10 @@ import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import androidx.annotation.AnimatorRes
+import androidx.annotation.DrawableRes
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.efedaniel.animationsdemo.R
 import com.efedaniel.animationsdemo.base.BaseFragment
@@ -17,6 +17,8 @@ class PropertyAnimationFragment : BaseFragment() {
 
     private lateinit var binding: FragmentPropertyAnimationBinding
     private lateinit var animator: Animator
+
+    @AnimatorRes private var animatorRes: Int = R.animator.property_alpha
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +32,39 @@ class PropertyAnimationFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar(getString(R.string.property_animation))
-
-        //TODO To set up completely later
-        animator = AnimatorInflater.loadAnimator(mainActivity, R.animator.property_rotate)
-        animator.setTarget(binding.targetImage)
+        setHasOptionsMenu(true)
         binding.rootLayout.setOnClickListener {
-            animator.start()
+            animator = AnimatorInflater.loadAnimator(mainActivity, animatorRes)
+            animator.run {
+                setTarget(binding.targetImage)
+                start()
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.property_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.alpha -> {
+                animatorRes = R.animator.property_alpha
+                true
+            }
+            R.id.rotate -> {
+                animatorRes = R.animator.property_rotate
+                true
+            }
+            R.id.scale -> {
+                animatorRes = R.animator.property_scale
+                true
+            }
+            R.id.translate -> {
+                animatorRes = R.animator.property_translate
+                true
+            }
+            else -> false
         }
     }
 }
