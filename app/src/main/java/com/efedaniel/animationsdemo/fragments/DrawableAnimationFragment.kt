1ba6dbca2b2270b6274c3cb74ad5e2478e8da09d
EfeDaniel <EfeEjemudaro@gmail.com>
2020-04-13 23:36:25 +0100

    Feature: Add start and stop controls on wifi animation

--- app/src/main/java/com/efedaniel/animationsdemo/fragments/DrawableAnimationFragment.kt
+++ app/src/main/java/com/efedaniel/animationsdemo/fragments/DrawableAnimationFragment.kt
@@ -1,10 +1,9 @@
 package com.efedaniel.animationsdemo.fragments
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.efedaniel.animationsdemo.R
 import com.efedaniel.animationsdemo.base.BaseFragment
@@ -13,6 +12,7 @@ import com.efedaniel.animationsdemo.databinding.FragmentDrawableAnimationBinding
 class DrawableAnimationFragment : BaseFragment() {
 
     private lateinit var binding: FragmentDrawableAnimationBinding
+    private lateinit var animation: AnimationDrawable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +26,24 @@ class DrawableAnimationFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar(getString(R.string.drawable_animation))
+//        setHasOptionsMenu(true)
+
+        binding.targetImage.setBackgroundResource(R.drawable.ic_signal_wifi_animation)
+        animation = binding.targetImage.background as AnimationDrawable
+
+        binding.rootLayout.setOnClickListener {
+            if (animation.isRunning) animation.stop() else animation.start()
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 
 }
